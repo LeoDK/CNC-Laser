f268d6d3c5f716f96330ffdc2a203967e2df7070
DK <34984338+LeoDK@users.noreply.github.com>
2018-02-24 15:12:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -29,32 +29,32 @@ machine = Machine(x, y, laser)
 ################################################################################################
 
 def search(line, char):
-	""" Cherche le charactère sur la ligne et retourne le nombre après """
-	char_location = line.index(char)+1
-	i = char_location
+    """ Cherche le charactère sur la ligne et retourne le nombre après """
+    char_location = line.index(char)+1
+    i = char_location
 
-	#En ascii, 48 : 0 et 57:9. Donc on arrête qd il y a plus de chiffres ni de points ni de signe
-	while ( 47<ord(line[i])<58 or line[i] == '.' or line[i] == '-' ):
-		i+=1
+    #En ascii, 48 : 0 et 57:9. Donc on arrête qd il y a plus de chiffres ni de points ni de signe
+    while ( 47<ord(line[i])<58 or line[i] == '.' or line[i] == '-' ):
+        i+=1
 
-	return float(line[char_location:i])
+    return float(line[char_location:i])
 
 def xyPosition(line):
-	""" Retourne les coordonnées x et y (pour une commande G0 ou G1) """
-	x = search(line, 'X')
-	y = search(line, 'Y')
-	return (x,y)
+    """ Retourne les coordonnées x et y (pour une commande G0 ou G1) """
+    x = search(line, 'X')
+    y = search(line, 'Y')
+    return (x,y)
 
 
 def ijPosition(line):
-	""" Même chose que pour xyPosition, mais avec I et J (G02 et G03) """
-	i = search(line, 'I')
-	j = search(line, 'J')
-	return (i,j)
+    """ Même chose que pour xyPosition, mais avec I et J (G02 et G03) """
+    i = search(line, 'I')
+    j = search(line, 'J')
+    return (i,j)
 
 
 def notYetImplError():
-	print "Sorry, functionnality not implemented yet. Exiting..."
+    print "Sorry, functionnality not implemented yet. Exiting..."
 
 
 ################################################################################################
@@ -64,103 +64,103 @@ def notYetImplError():
 ################################################################################################
 
 def execGCode(filename, machine):
-	
+    
 
-	try:
-		f = open(filename, 'r')
+    try:
+        f = open(filename, 'r')
 
-		for l in f:
-			#Si la ligne est vide
-			if not l:
-				pass
+        for l in f:
+            #Si la ligne est vide
+            if not l:
+                pass
 
-			#Instructions de position
-			#Position absolue
-			if l[0:3] == 'G90':
-				print "Absolute position"
-				machine.setAbsoluteMode()
+            #Instructions de position
+            #Position absolue
+            if l[0:3] == 'G90':
+                print "Absolute position"
+                machine.setAbsoluteMode()
 
-			#Position relative
-			elif l[0:3] == 'G91':
-				print "Relative position"
-				machine.setRelativeMode()
+            #Position relative
+            elif l[0:3] == 'G91':
+                print "Relative position"
+                machine.setRelativeMode()
 
-			#Unités -> pouces
-			elif l[0:3] == 'G20':
-				notYetImplError()
-				break
+            #Unités -> pouces
+            elif l[0:3] == 'G20':
+                notYetImplError()
+                break
 
-			#Unités -> mm
-			elif l[0:3] == 'G21':
-				print "Working in mm"
+            #Unités -> mm
+            elif l[0:3] == 'G21':
+                print "Working in mm"
 
-			#Retour a l'origine
-			elif l[0:3] == 'G28':
-				#Un seul axe...
-				if 'X' in l:
-					machine.home(axis=machine.x_axis)
-				if 'Y' in l:
-					machine.home(axis=machine.y_axis)
-				#... ou les deux si rien n'est précisé
-				if 'X' not in l and 'Y' not in l:
-					machine.home()
+            #Retour a l'origine
+            elif l[0:3] == 'G28':
+                #Un seul axe...
+                if 'X' in l:
+                    machine.home(axis=machine.x_axis)
+                if 'Y' in l:
+                    machine.home(axis=machine.y_axis)
+                #... ou les deux si rien n'est précisé
+                if 'X' not in l and 'Y' not in l:
+                    machine.home()
 
-			#Pause
-			elif l[0:3] == 'G4 ':
-				#En secondes
-				if 'S' in l:
-					t = search('S', l)
-					sleep(t)
-				#En ms
-				else:
-					t = search('P', l)
-					sleep(t*0.001)
+            #Pause
+            elif l[0:3] == 'G4 ':
+                #En secondes
+                if 'S' in l:
+                    t = search('S', l)
+                    sleep(t)
+                #En ms
+                else:
+                    t = search('P', l)
+                    sleep(t*0.001)
 
-			#Définition des coordonnées de l'origine
-			elif l[0:3] == 'G92':
-				notYetImplError()
-				break
+            #Définition des coordonnées de l'origine
+            elif l[0:3] == 'G92':
+                notYetImplError()
+                break
 
-			#Mouvement linéaire
-			elif l[0:3] == 'G0 ' or l[0:3] == 'G1 ' or l[0:3] == 'G01':
-				if 'F' not in l:
-					x,y = xyPosition(l)
-					machine.linearMovement(x, y)
+            #Mouvement linéaire
+            elif l[0:3] == 'G0 ' or l[0:3] == 'G1 ' or l[0:3] == 'G01':
+                if 'F' not in l:
+                    x,y = xyPosition(l)
+                    machine.linearMovement(x, y)
 
-			#Interpolation circulaire
-			elif l[0:3] == 'G02' or l[0:3] == 'G03':
-				x,y = xyPosition(l)
-				i,j = ijPosition(l)
-				#Sens horaire
-				if l[0:3] == 'G02':
-					machine.circularInterpolation(x, y, i, j, -1)
-				else:
-					machine.circularInterpolation(x, y, i, j, 1)
+            #Interpolation circulaire
+            elif l[0:3] == 'G02' or l[0:3] == 'G03':
+                x,y = xyPosition(l)
+                i,j = ijPosition(l)
+                #Sens horaire
+                if l[0:3] == 'G02':
+                    machine.circularInterpolation(x, y, i, j, -1)
+                else:
+                    machine.circularInterpolation(x, y, i, j, 1)
 
-			#Instructions du laser
-			elif l[0:3] == 'M03':
-				laser.on()
+            #Instructions du laser
+            elif l[0:3] == 'M03':
+                laser.on()
 
-			elif l[0:3] == 'M05':
-				laser.off()
+            elif l[0:3] == 'M05':
+                laser.off()
 
-			#Terminé
-			elif l[0:3] == 'M02':
-				break
+            #Terminé
+            elif l[0:3] == 'M02':
+                break
 
-	except IOError:
-		print "Couldn't open the file {}".format(filename)
+    except IOError:
+        print "Couldn't open the file {}".format(filename)
 
-	laser.off()
-	machine.release()
+    laser.off()
+    machine.release()
 
-	print "Done"
+    print "Done"
 
 try:
-	if len(sys.argv) != 2:
-		print "Usage : ./main.py [file]"
-		raise TypeError
-	gcode_file = sys.argv[1]
-	execGCode(gcode_file, machine)
+    if len(sys.argv) != 2:
+        print "Usage : ./main.py [file]"
+        raise TypeError
+    gcode_file = sys.argv[1]
+    execGCode(gcode_file, machine)
 except:
-	machine.release()
+    machine.release()
